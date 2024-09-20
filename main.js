603f0d293d8610facb8bb3e13180d2db7c653636
Michelangelo Calori <70917863+mikizdt@users.noreply.github.com>
2024-09-20 00:06:49 -0400

    adding basic Js logic

---
+++ main.js
@@ -0,0 +1,7 @@
+//variables
+const userInput = document.getElementById("userInput");
+const addBtn = document.getElementById("addBtn");
+const task = document.getElementById("task");
+const deleteBtn = document.getElementsByClassName("delete");
+
+addBtn.addEventListener("click", function () {});
